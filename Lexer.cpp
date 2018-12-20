88fa38da7d222eddf970bf98c9a8a163c2cd32d3
Lidor115 <lidor115@gmail.com>
2018-12-20 19:16:16 +0200

    Lexer

--- Lexer.cpp
+++ Lexer.cpp
@@ -2,10 +2,49 @@
 // Created by lidor on 19/12/18.
 //
 
-#include <string>
+#include "Lexer.h"
 
 using namespace std;
 
-string Lexer(string line){
 
+void checkRegex(list<string> &expretions, vector<string> &result, regex e);
+
+
+vector<string> LexerS(string line) {
+    list<string> expretions;
+    vector<string> result;
+    line = line + " ";
+    while (line != "") {
+        unsigned long firstSpace = line.find_first_of(' ');
+        unsigned long endOfstring = line.size() - firstSpace - 1;
+        string exp = line.substr(0, firstSpace);
+        expretions.push_back(exp);
+        line = line.substr(firstSpace + 1, endOfstring);
+    }
+    std::regex e("=|\\+|\\*|\\(|\\)|\\-|\\,|\\<|\\>|\\&|\\||\\!");
+
+    checkRegex(expretions, result, e);
+    return result;
 }
+
+void checkRegex(list<string> &expretions, vector<string> &result, regex e) {
+    std::smatch m;
+    // for each expresion divided by space
+    for (list<string>::iterator word = expretions.begin(); word != expretions
+            .end(); ++word) {
+        while (std::regex_search(*word, m, e)) {
+            if (m.prefix() != "") {
+                result.push_back(m.prefix().str());
+            }
+            for (auto x:m) {
+                result.push_back(x);
+            }
+            string check = m.prefix().str();
+            *word = m.suffix().str();
+        }
+        if (*word != "") {
+            result.push_back(*word);
+        }
+    }
+}
+
--- main.cpp
+++ main.cpp
@@ -6,14 +6,21 @@
 #include "ShuntingYard.h"
 #include <string>
 #include <iostream>
+#include <vector>
+#include "Lexer.h"
 
-using namespace std;
+int main(int argc, char *argv[]) {
 
-int main(){
+    string c = "";
+    for (int i = 1; i < argc; i++) {
+        c += argv[i];
+        c += " ";
+    }
+    vector<string> lex = LexerS(c);
     string str = " ( -2 * - 3 ) + - 5  * - 5";
     ShuntingYard s(str);
-    cout << s.evaluate () << "\n";
+    cout << s.evaluate() << "\n";
 
 
-return 0;
+    return 0;
 }
