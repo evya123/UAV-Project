6ffb011bb9d972812452ffc8eb4e86a59d71be1d
evya123 <evya123@gmail.com>
2018-12-27 09:28:58 -0800

    print command

--- Commends/PrintCommand.cpp
+++ Commends/PrintCommand.cpp
@@ -6,9 +6,9 @@
 #include "PrintCommand.h"
 void PrintCommand::doCommand(vector<string> &arguments, Data *d) {
     std::size_t i;
-    for (i = 0; i < arguments.size(); ++i){
+    for (i = arguments.size() - 1; i > 0; --i){
         std::cout << arguments[i]<<" ";
     }
-    std::cout<<"\n";
+    std::cout<<arguments[i]<<"\n";
     arguments.clear();
 }
